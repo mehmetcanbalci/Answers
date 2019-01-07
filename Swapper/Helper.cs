3c81bbab8f1bb993718364c6a8dddff263014d7a
Mehmetcan_b Mehmetcan_b <mehmetcanbalci@gmail.com>
2019-01-07 17:04:34 -0500

    v2

---
+++ Swapper/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapper
+{
+    public class Helper
+    {
+        public static void Swap(ref int x, ref int y)
+        {
+            x = x ^ y;
+            y = x ^ y;
+            x = x ^ y;
+        }
+    }
+}
---
+++ Swapper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapper
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,y;
+            do
+            {
+                Console.WriteLine("Enter First Variable :");
+
+            } while (!Int32.TryParse(Console.ReadLine(), out x));
+
+            do
+            {
+                Console.WriteLine("Enter Second Variable :");
+
+            } while (!Int32.TryParse(Console.ReadLine(), out y));
+
+            Helper.Swap(ref x, ref y);
+            Console.WriteLine("First Variable Value : {0}",x);
+            Console.WriteLine("Second Variable Value : {0}",y);
+            Console.ReadKey(); 
+        }
+    }
+}
